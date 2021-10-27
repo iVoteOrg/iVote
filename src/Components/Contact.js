c35d70cab128bf1a871dca453d3950c5a7de1d16
sahil2311sharma <sahilsharma123027@gmail.com>
2021-10-27 23:35:19 +0530

    Contact Send

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,20 +2,39 @@ import React from "react";
 import "./styles/contact.css";
 import Card from "react-bootstrap/Card";
 import Navbar from "./Navbar";
-
+import emailjs from "emailjs-com"
 
 export default class Contact extends React.Component{
     render(){
+        function sendEmail(e) 
+        {
+          e.preventDefault();
+          
+          emailjs.sendForm('service_kl0bsi8','template_dnz9j01',e.target,'user_ZcGC8TjgkvbwwxP1FiSnw')
+          .then(res=>{
+            console.log(res)
+          }).catch(res=>{
+            console.log(res)
+          })
+        }
         return(
-          <div id="contact">
-            <div className="center">
-              <Navbar/>
-              <Card.Title className="big">Contact</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
+          <div id="contact" className="center ">
+            <Navbar/>
+            <div className="container border" style={{marginTop:"50px",width:'50%',backgroundPosition:'center',backgroundSize:'cover'}}>
+              <h1 style = {{marginTop:'25px'}}>Contact Form</h1>
+              <form className = "row" style ={{margin:"25px 85px 75px 100px"}} onSubmit= {sendEmail}>
+                <label>First Name</label>
+                <input type='text' name = "name"/>
+                <br/>
+                <br/>
+                <label>Email</label>
+                <input type='email' name = "user_email" className ="form-control"/>
+                <br/>
+                <br/>
+                <label>Message</label>
+                <textarea name = "message" rows = '4' className ="form-control"/>
+                <input type = "submit" value = 'Send' className ="form-control btn btn-primary" style = {{marginTop: '30px'}}/>
+              </form>
             </div>
           </div>
         )
