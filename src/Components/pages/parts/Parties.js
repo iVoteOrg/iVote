2183aec812cd22177adc2bd4084467785a17d176
sahil2311sharma <sahilsharma123027@gmail.com>
2021-11-05 23:17:45 +0530

    parties added

--- src/Components/pages/parts/Parties.js
+++ src/Components/pages/parts/Parties.js
@@ -1,12 +1,25 @@
 import React from "react";
+import PartyCards from './PartyCards'
 import '../styles/parties.css'
 export default class About extends React.Component{
     render(){
         return(
         <div className = "parties">
             <h1 className ="h111">Present Running Elections</h1>
-            <div className ="">
-
+            <div className ="parties-container">
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                <PartyCards/>
+                
             </div>
         </div>
         )
---
+++ src/Components/pages/parts/PartyCards.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/parties.css'
+export default class About extends React.Component{
+    render(){
+        return(
+        <>
+            <div className ="card_box">
+                <div className ="party-logo"/>
+                <div className ="party-name">
+                    BJP
+                </div>
+            </div>
+        </>
+        )
+    }
+};
