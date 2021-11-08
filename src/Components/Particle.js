388aa7a867b8e8cda37c119de90f9c63dbaba8de
sahil2311sharma <sahilsharma123027@gmail.com>
2021-11-08 22:18:44 +0530

    particle page updated

--- src/Components/Particle.js
+++ src/Components/Particle.js
@@ -3,6 +3,16 @@ import "./particle/ParticleScreen.css";
 import ParticleBackground from "./particle/ParticleBackground";
 import Signinscroll from "./Signscroll";
 import Signin from "./Signin";
+import Typical from 'react-typical'
+const steps = [
+  'Hello👋',1000,
+  'Welcome to iVote',1000,
+  'We Concern Your Privacy 👌',1000,
+  "So that's why we build this platform ",1000,
+  "It's a 3 Factor Authentication System",1000,
+  "Each Vote Matters ♥",1000,
+  
+];
 export default class Particle extends React.Component{
     render(){
         return(
@@ -12,7 +22,9 @@ export default class Particle extends React.Component{
                         <ParticleBackground />
                         <div id="text_div center_all">
                             <div className="center_all">
-                                <h1 className="sasdasdasd">Welcome to <br />I-Vote</h1>
+                                <h1 className="sasdasdasd">
+                                <Typical wrapper="span" steps={steps} loop={Infinity} className={'caca'} />
+                                </h1>
                             </div>
                         </div>
                     </div>
